1341a315bb7e71d955285cd5f355ffc3cac78f9f
Rehno Lindeque <errantkid@gmail.com>
2019-03-16 14:05:19 -0400

    personal: helpful shell environment for compiling AMIs

---
+++ nixos/maintainers/scripts/ec2/shell.nix
@@ -0,0 +1,58 @@
+# { pkgs ? import <nixpkgs> {}
+{ pkgs ? import ../../../.. {}
+}:
+
+pkgs.stdenv.mkDerivation {
+  name = "ami-env";
+  version = "0.0.0";
+  buildInputs =
+    with pkgs;
+    [
+      direnv
+      awscli
+      # optional:
+      ec2_ami_tools
+      ec2_api_tools
+    ];
+  shellHook =
+    let
+      nc="\\e[0m"; # No Color
+      white="\\e[1;37m";
+      black="\\e[0;30m";
+      blue="\\e[0;34m";
+      light_blue="\\e[1;34m";
+      green="\\e[0;32m";
+      light_green="\\e[1;32m";
+      cyan="\\e[0;36m";
+      light_cyan="\\e[1;36m";
+      red="\\e[0;31m";
+      light_red="\\e[1;31m";
+      purple="\\e[0;35m";
+      light_purple="\\e[1;35m";
+      brown="\\e[0;33m";
+      yellow="\\e[1;33m";
+      grey="\\e[0;30m";
+      light_grey="\\e[0;37m";
+    in
+      ''
+      echo
+      printf "${white}"
+      echo '---------------------------------------------------'
+      echo 'AMI build environment'
+      echo '---------------------------------------------------'
+      printf "${nc}"
+      echo
+
+      export NIX_PATH=nixpkgs=${pkgs.path}
+
+      # Hook up direnv
+      if [ -n "$BASH_VERSION" ]; then
+        eval "$(direnv hook bash)"
+      elif [ -n "$ZSH_VERSION" ]; then
+        eval "$(direnv hook zsh)"
+      else
+        echo 'Unknown shell'
+      fi
+      '';
+}
+
